ab1365eaa7404c0c7e144ba97f1f044509adf9b5
UIT.19521334 <67258104+UIT19521334@users.noreply.github.com>
2022-07-06 16:05:27 +0700

    update

--- src/scenes/OnleaveScene/CreateLeave/index.tsx
+++ src/scenes/OnleaveScene/CreateLeave/index.tsx
@@ -208,7 +208,7 @@ const CreateLeave = () => {
 				/>
 				<Text style={[styles.titlle, { marginTop: 16 }]}>{getLabel('onleave.title_onleave_attach')}</Text>
 				<HStack>
-					<Image source={require('../../../assets/images/avatar.jpg')}
+					<Image source={require('../../../assets/images/demo.jpg')}
 						style={{
 							width: maxWidthActually *0.28,
 							resizeMode: 'contain',
